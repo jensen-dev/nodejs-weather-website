33ce539df2703ecb3278e887550c40a0922b5322
jensen-alimukti <jensen.alimukti@gmail.com>
2020-03-01 20:33:44 +0700

    forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/abc6015ae32274d1310b991f499a941a/'
-    + latitude + ',' + longitude + '?units=si'
+    const url = `https://api.darksky.net/forecast/abc6015ae32274d1310b991f499a941a/${latitude},${longitude}?units=si`
     
     request({url, json: true}, (error, {body}) => {
         if (error) {
@@ -12,9 +11,11 @@ const forecast = (latitude, longitude, callback) => {
         }else {
             const {temperature, precipProbability} = body.currently
             const dailyWeather = body.daily
-            callback(undefined, dailyWeather.data[0].summary + ' It is currently ' +
-            temperature + ' degrees celsius out. There is a ' +
-            precipProbability + ' % chance of rain.')
+            callback(undefined, `${dailyWeather.data[0].summary}
+                It is currently ${temperature} ⁰C out.
+                The high today is ${body.daily.data[0].temperatureHigh} ⁰C,
+                with a low of ${body.daily.data[0].temperatureLow} ⁰C.
+                There is a ${precipProbability} % chance of rain.`)
         }
     })
 }
